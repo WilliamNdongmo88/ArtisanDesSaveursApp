332f185d0529ab7bf5492c7a343f289af2a11016
William Ndongmo <williamndongmo88@gmail.com>
2025-07-17 05:28:31 +0200

    Gestion du panier

--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../models/product';
 import { Router } from '@angular/router';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-product-card',
@@ -14,7 +15,7 @@ export class ProductCardComponent {
   @Input() product!: Product;
   @Input() featured: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private cartService: CartService) {}
 
   onImageError(event: any) {
     // Fallback image en cas d'erreur de chargement
@@ -23,6 +24,13 @@ export class ProductCardComponent {
 
   navigateToDetails() {
     this.router.navigate(['/produit', this.product.id]);
+  }
+
+  addToCart() {
+    // Logique pour ajouter le produit au panier
+    // Par exemple, appeler un service de panier
+    this.cartService.addToCart(this.product);
+    //alert(`${this.product.name} ajouté au panier !`);
   }
 }
 
