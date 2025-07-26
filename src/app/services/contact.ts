cd65a40cd5f36888927e41c8930702d641530aed
William Ndongmo <williamndongmo88@gmail.com>
2025-07-26 06:10:42 +0200

    Communication entre service externe

--- src/app/services/contact.ts
+++ src/app/services/contact.ts
@@ -11,7 +11,7 @@ import { HttpClient } from '@angular/common/http';
 export class ContactService {
 
   //private apiUrl = 'http://localhost:8070/api/users';
-  private apiUrl = 'https://artisan-des-saveursp-production.up.railway.app/api/users';
+  private apiUrl = 'https://artisan-des-saveurs-production.up.railway.app/api/users';
 
   constructor(private http: HttpClient) { }
 
